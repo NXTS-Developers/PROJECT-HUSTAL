420b2866c719b70ab08c4ce5f1a9a95ce2644603
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-09-14 09:51:03 +0500

    Update createaccount.php

--- clientarea/function/createaccount.php
+++ clientarea/function/createaccount.php
@@ -5,13 +5,15 @@ require "../../vendor/autoload.php";
 use \HansAdema\AnakeClient\Client;
 include "connect.php";
 $s=mysqli_query($connect,"select * from vhost_cpanel where client_id='".$_POST['id']."'");
-if(mysqli_num_rows($s)<=3){
+if(mysqli_num_rows($s)<4){
 // create account
 if(isset($_POST['create'])){
 // setting up array
+$UserName=substr(str_shuffle('0123456789QWRTYUIOPELKJHGFDSAZXCVBNMzxcvbnmlkjhgfdsaqwertyuiop'),0,8);
+$Password=substr(str_shuffle('0123456789QWRTYUIOPELKJHGFDSAZXCVBNMzxcvbnmlkjhgfdsaqwertyuiop'),0,16);
 $tsData=array(
-'username'=>$_POST['username'],
-'password'=>$_POST['password'],
+'username'=>$UserName,
+'password'=>$Password,
 'domain'=>$_POST['domain'],
 'email'=>$_COOKIE['client'],
 'plan'=>API_PLAN,
@@ -29,9 +31,9 @@ $request=$client->createAccount([
 // sending request
 $response=$request->send();
 //receiving respond
-elseif((int)$response->isSuccessful()==1&&strlen($response->getMessage())>1)
+if((int)$response->isSuccessful()==1&&strlen($response->getMessage())>1)
 $result="1";
-if((int)$response->isSuccessful()==0&&strlen($response->getMessage())>1)
+elseif((int)$response->isSuccessful()==0&&strlen($response->getMessage())>1)
 $result="0";
 elseif((int)$response->isSuccessful()==0&&(int)$response->getMessage()==0)
 $result="2";
@@ -40,12 +42,11 @@ $uname=$response->getVpUsername();
 $passwd=$tsData['password'];
 if($result==0){
 $_SESSION['msg']="<div class='alert alert-warning' role='alert'>".$response->getMessage()."</div>";
-$sql=mysqli_query($connect,"insert into vhost_cpanel(client_id,cpanel_username,cpanel_client_username,cpanel_password,cpanel_status,cpanel_date,cpanel_label,cpanel_domain) values ('".$_POST['id']."','".$response->getVpUsername()."','".$tsData['username']."','".$tsData['password']."','1','".date("d-m-Y")."','".$_POST['label']."','".$_POST['domain']."')");
-include "../mail/newaccount.php";
 header("location: ../accounts.php"); 
 }
 if($result==1){
 $sql=mysqli_query($connect,"insert into vhost_cpanel(client_id,cpanel_username,cpanel_client_username,cpanel_password,cpanel_status,cpanel_date,cpanel_label,cpanel_domain) values ('".$_POST['id']."','".$response->getVpUsername()."','".$tsData['username']."','".$tsData['password']."','1','".date("d-m-Y")."','".$_POST['label']."','".$_POST['domain']."')");
+$_SESSION['msg']="<div class='alert alert-success' role='alert'>Account  created successfully</div>";
 include "../mail/newaccount.php";
 header("location: ../accounts.php"); 
 }
