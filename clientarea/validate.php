23190ddf411a681d5ae8ea6aab366974e2056e5a
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-09-14 09:51:27 +0500

    Add files via upload

---
+++ clientarea/validate.php
@@ -0,0 +1,34 @@
+<?php
+include 'includes/connect.php';
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<!--meta information-->
+<title><?php echo "Validate - ".SITE_NAME;?></title>
+<meta chrset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta name="description" content="<?php echo 'Validation page for '.SITE_NAME;?>">
+<meta name="og:description" content="">
+<meta name="og:type" content="website">
+<meta name="og:url" content="<?php echo $_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];?>">
+<meta name="og:image" content="https://<?php echo $_SERVER['HTTP_HOST'];?>/template/image/icon.png">
+<!--styles and scripts-->
+<link rel='icon' href='../template/image/icon.png' type="image/png" sizes="16x16">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" />
+<link rel="stylesheet" href="../template/css/bootstrap.min.css">
+<script src="../template/js/jquery.js"></script>
+<script src="../template/js/popper.min.js"></script>
+<script src="../template/js/bootstrap.min.js"></script>
+</head>
+<body class="bg-white container">
+<div class="row">
+<div class="offset-md-3 col-md-6 my-5">
+<div class="mx-3 my-5 card card-body text-center">
+<h3>Validation Error</h3>
+<p>You need to validate your account before you can use our free hosting service please click on the link that had been sent your registered email address.</p>
+</div>
+</div>
+</div>
+</body>
+</html>
