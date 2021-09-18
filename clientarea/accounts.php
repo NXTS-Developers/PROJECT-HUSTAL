b28cb5b22057bbde29528a4a009b0585746e5553
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-09-18 21:54:32 +0500

    Update accounts.php

--- clientarea/accounts.php
+++ clientarea/accounts.php
@@ -10,7 +10,7 @@ $sql=mysqli_query($connect,"select * from vhost_cpanel where client_id='".$user[
 .regAlert{background-color:#ffdfdf;color:#48525c}
 .regAlert:before{background: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='1 0 22 22' fill='none' stroke='%2348525c' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5'><path d='M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z'/><line x1='12' y1='9' x2='12' y2='13'/><line x1='12' y1='17' x2='12.01' y2='17'/></svg>") center / 20px no-repeat;}
 </style>
-<div class="container">
+<div class="container"><br>
 <div class="card shadow-sm rounded">
 <div class="card-body">
 <h5 class="text-dark pull-left"><b>Accounts</b></h5>
