cecf6c9bb3005576d5576d1c98b42eca068974c3
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-09-18 17:18:18 +0500

    Update home.php

--- clientarea/home.php
+++ clientarea/home.php
@@ -48,28 +48,6 @@ echo "<br><b>Service Domain: </b>".SITE_ADDR;
 </div>
 </div>
 </div><br>
-<script>
-var deadline = new Date("4 26, 2021 12:00:00").getTime(); 
-var x = setInterval(function() { 
-var now = new Date().getTime(); 
-var t = deadline - now; 
-var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
-var seconds = Math.floor((t % (1000 * 60)) / 1000); 
-document.getElementById("day").innerHTML =days ; 
-document.getElementById("hour").innerHTML =hours; 
-document.getElementById("minute").innerHTML = minutes;  
-document.getElementById("second").innerHTML =seconds;  
-if (t < 0) { 
-        clearInterval(x); 
-        document.getElementById("demo").innerHTML = "TIME UP"; 
-        document.getElementById("day").innerHTML ='0'; 
-        document.getElementById("hour").innerHTML ='0'; 
-        document.getElementById("minute").innerHTML ='0' ;  
-        document.getElementById("second").innerHTML = '0'; } 
-}, 1000);
-</script>
 </div>
 </div>
 </div>
