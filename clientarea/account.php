357860278fcd06c91d42038325ce0e21cb8c16a7
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-09-25 22:39:08 +0500

    Update account.php

--- clientarea/account.php
+++ clientarea/account.php
@@ -20,7 +20,7 @@ $hash=base64_encode($pwd);
 <div class="list-group-item">
 <div class="row"><?php if($sql['cpanel_status']==1){?>
 <div class="col-lg-4 col-md-4 d-grid">
-<a href="cpanel.php?aid=<?php echo $_GET['aid'];?>" target="_blank" class="btn btn-success">Control Panel</a>
+<a href="cpanel.php?aid=<?php echo $_GET['aid'];?>" target="_blank" class="btn btn-success">Condivol Panel</a>
 </div>
 <div class="col-lg-4 col-md-4 d-grid">
 <a href="https://filemanager.ai/new/#/c/ftpupload.net/<?php echo $sql['cpanel_username']."/".$hash?>" target="_blank" class="btn btn-primary">File Manager</a>
@@ -44,32 +44,32 @@ $hash=base64_encode($pwd);
 <div class="list-group-item fs-5 text-dark">
 Account Details
 </div>
-<table>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>Username</b><?php echo $sql['cpanel_username'];?></small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>Password</b><?php echo $sql['cpanel_password'];?> </small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>Status</b><?php if($sql['cpanel_status']==1){echo "<span class='badge bg-success'>Active</span>";}else{ if($sql['cpanel_status']==2){echo "<span class='badge bg-warning'>Inactive</span>";}else{echo "<span class='badge bg-danger'>Suspended</span>";} }?></small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b> Label  </b><?php echo $sql['cpanel_label'];?> </small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>Main Domain</b><?php echo $sql['cpanel_client_username'].".".SITE_ADDR;?></small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>IP Address</b><?php require_once "../vendor/user_info/UserInfo.php";echo UserInfo::get_ip();?></small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>Server IP</b>185.27.134.46 </small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted"><b>Date Created</b><?php echo $sql['cpanel_date'];?> </small></td>
-</tr>
-</table>
+<div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>Username</b><?php echo $sql['cpanel_username'];?></div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>Password</b><?php echo $sql['cpanel_password'];?> </div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>Status</b><?php if($sql['cpanel_status']==1){echo "<span class='badge bg-success'>Active</span>";}else{ if($sql['cpanel_status']==2){echo "<span class='badge bg-warning'>Inactive</span>";}else{echo "<span class='badge bg-danger'>Suspended</span>";} }?></div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b> Label  </b><?php echo $sql['cpanel_label'];?> </div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>Main Domain</b><span><?php echo $sql['cpanel_client_username'].".".SITE_ADDR;?></span></div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>IP Address</b><?php require_once "../vendor/user_info/UserInfo.php";echo UserInfo::get_ip();?></div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>Server IP</b>185.27.134.46 </div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted"><b>Date Created</b><?php echo $sql['cpanel_date'];?> </div></div>
+</div>
+</div>
 </div>
 </div>
 <div class="col-lg-6 col-md-6"><br>
@@ -77,39 +77,39 @@ Account Details
 <div class="list-group-item fs-5 text-dark">
 MySQL Details
 </div>
-<table>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>MySQL Username</b><?php echo $sql['cpanel_username'];?> </small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>MySQL Password</b><?php echo $sql['cpanel_password'];?> </small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>MySQL Hostname</b>sqlxxx.byethost.org</small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>MySQL Port</b>3306</small></td>
-</tr>
-</table>
+<div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>MySQL Username</b><?php echo $sql['cpanel_username'];?> </div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>MySQL Password</b><?php echo $sql['cpanel_password'];?> </div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>MySQL Hostname</b>sqlxxx.byethost.org</div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>MySQL Port</b>3306</div></div>
+</div>
+</div>
 </div><br>
 <div class="list-group shadow-sm rounded">
 <div class="list-group-item fs-5 text-dark">
 FTP Details
 </div>
-<table>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>FTP Username</b><?php echo $sql['cpanel_username'];?> </small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>FTP Password</b><?php echo $sql['cpanel_password'];?> </small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>FTP Hostname</b>ftpupload.net</small></td>
-</tr>
-<tr class="list-group-item">
-<td><small class="text-muted d-flex justify-content-between align-items-center"><b>FTP Port</b>21</small></td>
-</tr>
-</table>
+<div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>FTP Username</b><?php echo $sql['cpanel_username'];?> </div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>FTP Password</b><?php echo $sql['cpanel_password'];?> </div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>FTP Hostname</b>ftpupload.net</div></div>
+</div>
+<div class="list-group-item">
+<div><div class="text-muted d-flex justify-content-between align-items-center"><b>FTP Port</b>21</div></div>
+</div>
+</div>
 </div><br>
 </div><br> 
 </div>
